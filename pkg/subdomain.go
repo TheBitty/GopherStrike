7cab73275c4a5f9b2c5d989b226494da8b1170d6
Bitty <137277320+TheBitty@users.noreply.github.com>
2025-03-06 12:11:48 -0800

    Update subdomain.go

--- pkg/subdomain.go
+++ pkg/subdomain.go
@@ -1,1 +1,11 @@
 package pkg
+
+import (
+	"GopherStrike/pkg/tools"
+)
+
+// RunSubdomainScannerWithCheck executes the subdomain scanner
+func RunSubdomainScannerWithCheck() error {
+	// Call the actual implementation from tools' package
+	return tools.RunSubdomainScan()
+}
